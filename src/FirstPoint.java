12a711a076a94250e938bd1fed7f05ca2f2efb83
mirana <golovanshalom@gmail.com>
2023-03-13 21:20:43 +0400

    CF and NCF calculations

--- src/FirstPoint.java
+++ src/FirstPoint.java
@@ -13,4 +13,33 @@ public class FirstPoint {
         for(double elem : arr) res += Math.pow(elem - average,2);
         return res / arr.size();
     }
+
+    public static double correlationF(int m, ArrayList<Double> arr){
+        if (m < 0) m *= -1;
+        ArrayList<Double> arr1 = new ArrayList<>();
+        ArrayList<Double> arr2 = new ArrayList<>();
+        double res = 0;
+        for(int i = 0; i < arr.size() - m; i++){
+            arr1.add(arr.get(i));
+            arr2.add(arr.get(i + m));
+        }
+        double arr1Av = average(arr1);
+        double arr2Av = average(arr2);
+        for(int i = 0; i < arr.size() - m; i++) res += (arr1.get(i) - arr1Av) * (arr2.get(i) - arr2Av);
+        return res / (arr.size() - m);
+    }
+
+    public static double NCF(int m, ArrayList<Double> arr){
+        if (m < 0) m *= -1;
+        ArrayList<Double> arr1 = new ArrayList<>();
+        ArrayList<Double> arr2 = new ArrayList<>();
+        for(int i = 0; i < arr.size() - m; i++){
+            arr1.add(arr.get(i));
+            arr2.add(arr.get(i + m));
+        }
+        double arr1D = dispersion(average(arr1), arr1);
+        double arr2D = dispersion(average(arr2), arr2);
+        double corr = correlationF(m, arr);
+        return corr / Math.sqrt(arr1D * arr2D);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException{
         ArrayList<Double> list = new ArrayList<>();
         list = readFile(list);
-        double average = FirstPoint.average(list);
-        double dispersion = FirstPoint.dispersion(average, list);
-        System.out.print(average + " " + dispersion);
+        double CF, NCF;
+        DecimalFormat df = new DecimalFormat("#.#####");
+        df.setRoundingMode(RoundingMode.CEILING);
+        for(int i = 0; i < 15; i++){
+            CF = FirstPoint.correlationF(i, list);
+            NCF = FirstPoint.NCF(i, list);
+            System.out.println(i + ": CF " + df.format(CF) + " : NCF " + df.format(NCF));
+        }
     }
 
-    public static ArrayList<Double> readFile(ArrayList<Double> list) throws IOException, ClassNotFoundException {
+    public static ArrayList<Double> readFile(ArrayList<Double> list) throws IOException{
         File file = new File("C:\\Users\\Anna\\IdeaProjects\\tcpLab\\src\\45.txt");
         FileReader reader = new FileReader(file);
         Scanner sc = new Scanner(reader);
